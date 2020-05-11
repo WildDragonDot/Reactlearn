0c7296c7bcd3aa2c0245a2c9062d50ce21ceaee6
Chandan <vishwakarmachandan336@gmail.com>
2020-05-11 15:38:26 +0530

    add all changes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 
 
 ReactDOM.render(  
-  <h1>Chnadan Vishwakarma</h1>, //this is jSx not a html//
-
+  [
+  <h1>Chnadan Vishwakarma</h1>,
+  <p>Please like and share</p>,
+  <h2>I am don and I am not a terorist</h2>,
+  <h3>I am the best</h3>
+  ],
   document.getElementById('root')
 );
 
