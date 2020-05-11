137735e0f21bf4900930e8445d3e35f219ba8240
Chandan <vishwakarmachandan336@gmail.com>
2020-05-11 18:56:00 +0530

    Chanage in evening

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 
 
 ReactDOM.render(  
-  [
-  <h1>Chnadan Vishwakarma</h1>,
-  <p>Please like and share</p>,
-  <h2>I am don and I am not a terorist</h2>,
+  <React.Fragment>
+  <h1>Chandan Vishwakarma</h1>
+  <p>Please like and share</p>
+  <h2>I am don and I am not a terorist</h2>
   <h3>I am the best</h3>
-  ],
+  </React.Fragment>,
   document.getElementById('root')
 );
 
