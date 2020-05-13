f7327d939c310c73cf1ff2948f2b1659c5742573
Chandan <vishwakarmachandan336@gmail.com>
2020-05-14 01:21:10 +0530

    master3 update

--- src/index.js
+++ src/index.js
@@ -2,34 +2,36 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
-const fname="Chandan";
-const lname="Vishwakarma";
-const currdate=new Date().toLocaleDateString();
-const currtime=new Date().toLocaleTimeString();
-const img1="https://picsum.photos/200/300";
-const img2="https://picsum.photos/300/300";
-const img3="https://picsum.photos/400/300";
-const img4="https://picsum.photos/500/300";
-const img5="https://picsum.photos/600/300";
 
-// inline css property
-const beauty={
-  border:'1px dotted yellow',
-  textAlign:'center'
+let greeting = '';
+const cssStyle={
+  
 };
+const beauty={
+  textAlign:'center',
+fontFamily:'Arial'
+}
 
-//
+let curDate = new Date();
+curDate=curDate.getHours();
+if(curDate >=1 && curDate<12){
+ greeting='Good Morning';
+ cssStyle.color='green';
+}
+else if(curDate>12 && curDate<19){
+  greeting='Good Afternoon';
+  cssStyle.color='orange';
+}
+else{
+  greeting='Good Night';
+  cssStyle.color='black';
+}
+
 ReactDom.render(
   <React.Fragment>
-    <h1 className="heading" style={beauty}> {`${fname} ${lname}`}</h1>
-    {/* <p>Current Date is = {currdate}</p> 
-    <p>Current Time is = {currtime}</p>     */}
-    <div className="imgdiv">
-    <img alt="RandomImages" src={img1}/>
-    <img alt="RandomImages" src={img2}/>
-    <img alt="RandomImages" src={img3}/>
-    <img alt="RandomImages" src={img4}/>
-    <img alt="RandomImages" src={img5}/></div>
+ 
+<h1 style={beauty}>Hello sir, {greeting}</h1>
+
   </React.Fragment>,
   document.getElementById('root')
 );
